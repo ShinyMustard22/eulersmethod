de292c81dfd37b928693c5f365d39728a8582313
Shubham Parab <71990977+Skparab1@users.noreply.github.com>
2023-02-16 16:34:00 -0800

    math math.eval using math.js

--- app.js
+++ app.js
@@ -6,6 +6,28 @@ const dx = document.getElementById('dx');
 const xn = document.getElementById('xn');
 const answer = document.getElementById('answer');
 
+// this processes the facking equation and
+function calculate(x,y){
+
+    let lucidair = dydx.value;
+
+    //lucidair = lucidair.replace(/[^0-9\xy(\)\+\-\*\/\.]/g, ""); // probably dont need this
+
+    lucidair = lucidair.toLowerCase();
+
+    let thex = x;
+    let they = y;
+
+    lucidair = lucidair.replaceAll('x',thex);
+    lucidair = lucidair.replaceAll('y',they);
+
+    console.log('evaluating '+lucidair);
+
+    lucidair = math.evaluate(lucidair);
+
+    return lucidair;
+}
+
 submit.addEventListener('click', () => {
     const dydxc = "x - y";
     let x0c = parseFloat(x0.value);
@@ -19,5 +41,14 @@ submit.addEventListener('click', () => {
         ync += (i - ync) * dxc;
     }
 
-    answer.innerHTML = "Answer: y(" + xnc + ") ≈ " + ync;
+    //answer.innerHTML = "Answer: y(" + xnc + ") ≈ " + ync;
+
+    let evalx = x0.value;
+    let evaly = y0.value;
+
+    let resp = calculate(evalx,evaly);
+
+    answer.innerHTML = '('+evalx+','+evaly+') with '+dydx.value+' = '+resp;
+
+    console.log(processeq());
 });
